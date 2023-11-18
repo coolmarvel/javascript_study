cdcc0ee69c1c80a655b5f56cabdc66719b8a88e2
coolmarvel <marvel97@naver.com>
2023-11-18 22:41:01 +0900

    res.json vs res.send commit

--- 4_chapter/express/server.js
+++ 4_chapter/express/server.js
@@ -13,8 +13,7 @@ const users = [
 const app = express(); // 새로운 Express App 생성
 
 app.get("/", (req, res) => {
-  // "/" 이 경로로 요청이 오면 Hello World를 결과값으로 전달
-  res.send("Hello World");
+  res.send({ a: "a" });
 });
 
 app.get("/users", (req, res) => {
@@ -34,3 +33,60 @@ app.get("/users/:userId", (req, res) => {
 
 app.listen(PORT, HOST); // 해당 포트와 호스트에서 HTTP 서버를 시작
 console.log(`Running on http://${HOST}:${PORT}`);
+
+/**
+ * res.json() 소드 코드
+ *
+ * res.json = function json(obj) {
+ *  var val = obj;
+ *
+ *  // 생략...
+ *
+ *  // settings
+ *  var app = this.app;
+ *  var escape = app.get('json escape');
+ *  var replacer = app.get('json replace');
+ *  var spaces = app.get('json spaces');
+ *  var body = stringify(val, replacer, spaces, escape);
+ *
+ *  // content-type
+ *  if (!this.get('Content-Type')) {
+ *   this.set('Content-Type', 'application/json');
+ *  }
+ *
+ *  return this.send(body);
+ * }
+ */
+
+/**
+ * res.send = function send(body) {
+  var chunk = body;
+
+  // ...
+
+  switch (typeof chunk) {
+    case "string":
+      if (!this.get("Content-Type")) {
+        this.type("html");
+      }
+      break;
+
+    case "boolean":
+    case "number":
+    case "object":
+      if (chunk === null) {
+        chunk = "";
+      } else if (Buffer.isBuffer(chunk)) {
+        if (!this.get("Content-Type")) {
+          this.type("bin");
+        }
+      } else {
+        return this.json(chunk);
+      }
+    default:
+      break;
+  }
+
+  return this;
+};
+ */
