6c04b6607f09ba74b9a14a8a6ec73eb3a215b25f
coolmarvel <marvel97@naver.com>
2023-11-18 22:26:12 +0900

    router request params commit

--- 4_chapter/express/server.js
+++ 4_chapter/express/server.js
@@ -4,12 +4,32 @@ const express = require("express"); // Express 모듈 불러오기
 const PORT = 8080; // Express 서버를 위한 포트 설정
 const HOST = "0.0.0.0"; // 호스트 지정
 
+const users = [
+  { id: 0, name: "Jack" },
+  { id: 1, name: "Jennifer" },
+];
+
 // App
 const app = express(); // 새로운 Express App 생성
 
 app.get("/", (req, res) => {
   // "/" 이 경로로 요청이 오면 Hello World를 결과값으로 전달
   res.send("Hello World");
+});
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.get("/users/:userId", (req, res) => {
+  const userId = Number(req.params.userId);
+  const user = users[userId];
+
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    res.status(404).json({ error: "No User Found" });
+  }
 });
 
 app.listen(PORT, HOST); // 해당 포트와 호스트에서 HTTP 서버를 시작
