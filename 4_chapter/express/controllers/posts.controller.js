aada847de8b09bccefd203b5ef6b48dd972665cc
coolmarvel <marvel97@naver.com>
2023-11-19 23:00:53 +0900

    template engine layout edit commit

--- 4_chapter/express/controllers/posts.controller.js
+++ 4_chapter/express/controllers/posts.controller.js
@@ -4,7 +4,7 @@ function getPost(req, res) {
   // path.join은 여러 세그먼트를 하나의 경로로 결합
   // res.sendFile(path.join(__dirname, "..", "public", "images", "profile.png")); // __dirname은 현재 실행하는 파일의 절대 경로
 
-  res.render("index", { imageTitle: "It is a forest" });
+  res.render("posts", { templateName: "post" });
 }
 
 module.exports = { getPost };
