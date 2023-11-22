ca4ded5e54a437e288f8eb0d49a58c9febce1c73
coolmarvel <marvel97@naver.com>
2023-11-22 10:45:23 +0900

    using hbs module views app edit & express app create

--- 8_chapter/mongoose/server.js
+++ 8_chapter/mongoose/server.js
@@ -1,4 +1,7 @@
-const app = require("express")();
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
 
 // app.get("*", (req, res, next) => {
 //   throw new Error("woops");
@@ -18,14 +21,53 @@ const app = require("express")();
 //   // 그래서 첫 번째 미들웨어에서 발생한 에러 메시지를 이곳에서 처리해줍니다.
 // });
 
-app.get("*", (req, res, next) => {
-  setImmediate(() => {
-    throw next(new Error("woops"));
-  });
+// app.get("*", (req, res, next) => {
+//   setImmediate(() => {
+//     throw next(new Error("woops"));
+//   });
+// });
+
+// app.use((error, req, res, next) => {
+//   res.send({ message: error.message });
+// });
+
+// app.listen(3000);
+
+const path = require("path");
+
+const usersRouter = require("./routes/users.router");
+const postsRouter = require("./routes/posts.router");
+const productsRouter = require("./routes/products.router");
+
+const mongoose = require("mongoose");
+
+const PORT = 3000;
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use("/static", express.static(path.join(__dirname, "public")));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  const start = Date.now();
+  console.log(`${req.method} ${req.url}`);
+  next();
+  const diffTime = Date.now() - start;
+  console.log(`${req.method} ${req.baseUrl}${req.url} ${diffTime}ms`);
 });
 
-app.use((error, req, res, next) => {
-  res.send({ message: error.message });
-});
+app.use("/users", usersRouter);
+app.use("/posts", postsRouter);
+app.use("/products", productsRouter);
 
-app.listen(3000);
+const { MONGO_USERNAME, MONGO_PASSWORD } = process.env;
+
+mongoose
+  .connect(`mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@test.vi6echp.mongodb.net/`)
+  .then(() => console.log("mongodb connected"))
+  .catch((err) => console.log(err));
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}...`);
+});
