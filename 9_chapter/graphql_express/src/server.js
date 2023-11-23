67555e208a6441879d23634c4787401fa26e77ad
coolmarvel <marvel97@naver.com>
2023-11-23 12:56:41 +0900

    apollo server v3 migrate apollo server v4

--- 9_chapter/graphql_express/src/server.js
+++ 9_chapter/graphql_express/src/server.js
@@ -3,7 +3,11 @@ const path = require("path");
 
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 const { loadFilesSync } = require("@graphql-tools/load-files");
-const { ApolloServer } = require("apollo-server-express");
+
+const { expressMiddleware } = require("@apollo/server/express4");
+const { ApolloServer } = require("@apollo/server");
+const { json } = require("body-parser");
+const cors = require("cors");
 
 const loadedTypes = loadFilesSync("**/*", { extensions: ["graphql"] });
 const loadedResolvers = loadFilesSync(path.join(__dirname, "**/*.resolver.js"));
@@ -26,7 +30,7 @@ async function startApolloServer() {
   await server.start();
 
   // Connect apollo middleware with express server
-  server.applyMiddleware({ app, path: "/graphql" });
+  app.use("/graphql", cors(), json(), expressMiddleware(server, { context: async ({ req }) => ({ token: req.headers.token }) }));
 
   app.listen(port, () => {
     console.log(`Apollo Server listening on http://localhost:${port}`);
