f9bd3c2bb91866ce43c3cfe49c84231393c845f1
coolmarvel <marvel97@naver.com>
2023-11-23 12:52:06 +0900

    graphql-express remove & apollo-server-express v3 init

--- 9_chapter/graphql_express/src/server.js
+++ 9_chapter/graphql_express/src/server.js
@@ -1,31 +1,36 @@
 const express = require("express");
+const path = require("path");
 
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 const { loadFilesSync } = require("@graphql-tools/load-files");
-const { graphqlHTTP } = require("express-graphql");
-const { buildSchema } = require("graphql");
-
-const path = require("path");
+const { ApolloServer } = require("apollo-server-express");
 
 const loadedTypes = loadFilesSync("**/*", { extensions: ["graphql"] });
-
 const loadedResolvers = loadFilesSync(path.join(__dirname, "**/*.resolver.js"));
 
-const schema = makeExecutableSchema({
-  typeDefs: loadedTypes,
-  resolvers: loadedResolvers,
-});
+async function startApolloServer() {
+  // Required logic for integrating with Express
+  const app = express();
+  const port = 3000;
 
-const root = {
-  posts: require("./posts/posts.model"),
-  comments: require("./comments/comments.model"),
-};
+  const schema = makeExecutableSchema({
+    typeDefs: loadedTypes,
+    resolvers: loadedResolvers,
+  });
 
-const app = express();
-const port = 3000;
+  // This Apollo server object contains all th middleware
+  // and logic to handle incoming graphical requests.
+  const server = new ApolloServer({ schema });
 
-app.use("/graphql", graphqlHTTP({ schema: schema, graphiql: true }));
+  // Ensure we wait for our apollo server to start
+  await server.start();
 
-app.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}`);
-});
+  // Connect apollo middleware with express server
+  server.applyMiddleware({ app, path: "/graphql" });
+
+  app.listen(port, () => {
+    console.log(`Apollo Server listening on http://localhost:${port}`);
+  });
+}
+
+startApolloServer();
