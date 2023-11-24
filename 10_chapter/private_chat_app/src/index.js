91f240a3b082e308243386c06f07050bbde91b10
coolmarvel <marvel97@naver.com>
2023-11-24 11:28:03 +0900

    socket.io connect

--- 10_chapter/private_chat_app/src/index.js
+++ 10_chapter/private_chat_app/src/index.js
@@ -1,6 +1,28 @@
 const express = require("express");
 const path = require("path");
 
+const http = require("http");
+const { Server } = require("socket.io");
+const server = http.createServer(app);
+const io = new Server(server);
+
+// socket events
+let users = [];
+io.on("connection", async (socket) => {
+  // get all users
+  let userData = {};
+  users.push(userData);
+  io.emit("users-data", { users });
+
+  // get message from client
+  socket.io("message-to-server", () => {});
+
+  // fetch previous messages
+  socket.on("fetch-messages", () => {});
+
+  socket.on("disconnect", () => {});
+});
+
 const app = express();
 
 const publicDir = path.join(__dirname, "../public");
@@ -8,6 +30,6 @@ const publicDir = path.join(__dirname, "../public");
 app.use(express.json());
 app.use(express.static(publicDir));
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log(`Server is running on http://localhost:3000`);
 });
