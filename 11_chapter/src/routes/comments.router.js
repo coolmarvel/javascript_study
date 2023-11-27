c8cd0e8ab06eeedd38b55a76cce061afb705ee70
coolmarvel <marvel97@naver.com>
2023-11-27 11:20:11 +0900

    comments delete & put logic handler update

--- 11_chapter/src/routes/comments.router.js
+++ 11_chapter/src/routes/comments.router.js
@@ -30,4 +30,36 @@ router.post("/", checkAuthenticated, (req, res) => {
   });
 });
 
+router.delete(":commentId", checkCommentOwnership, (req, res) => {
+  CommentModel.findByIdAndDelete(req.params.commentId, (err, _) => {
+    if (err) req.flash("error", "댓글 삭제 중 에러가 발생했습니다.");
+    else req.flash("success", "댓글을 삭제했습니다.");
+
+    res.redirect("back");
+  });
+});
+
+router.get("/:commentId/edit", checkCommentOwnership, (req, res) => {
+  PostModel.findById(req.params.id, (err, post) => {
+    if (err || !post) {
+      req.flash("error", "댓글에 해당하는 게시글이 없거나 에러가 발생했습니다.");
+      res.redirect("back");
+    } else {
+      res.render("comments/edit", { post: post, comment: req.comment });
+    }
+  });
+});
+
+router.put("/:commentId", checkCommentOwnership, (req, res) => {
+  CommentModel.findByIdAndUpdate(req.params.commentId, req.body, (err, _) => {
+    if (err) {
+      req.flash("error", "댓글을 수정하는데 에러가 났습니다.");
+      res.redirect("back");
+    } else {
+      req.flash("success", "댓글을 수정하는데 성공했습니다.");
+      res.redirect("/posts");
+    }
+  });
+});
+
 module.exports = router;
