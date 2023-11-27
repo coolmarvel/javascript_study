1e3681e84a9683aeec94631d0ec174d20be83e6f
coolmarvel <marvel97@naver.com>
2023-11-27 16:00:00 +0900

    basic express setting

--- 12_chapter/src/server.js
+++ 12_chapter/src/server.js
@@ -1,13 +1,72 @@
 const express = require("express");
 const app = express();
 
+require("dotenv").config();
 const path = require("path");
+const config = require("config");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const flash = require("connect-flash");
+const session = require("express-session");
+const cookieSession = require("cookie-session");
+const fileUpload = require("express-fileupload");
+const methodOverride = require("method-override");
 
+const server_config = config.get("server");
+const port = server_config.port;
+
+const { COOKIE_ENCRYPTION_KEY, MONGO_URI } = process.env;
+
+app.use(
+  session({
+    secret: "session_secret",
+    name: "shop-app-cookie",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { httpOnly: true, secure: false },
+  })
+);
+app.use(
+  cookieSession({
+    name: "cookie-session",
+    keys: [COOKIE_ENCRYPTION_KEY],
+  })
+);
+app.use((req, res, next) => {
+  if (req.session && !req.session.regenerate) req.session.regenerate = (cb) => cb();
+  if (req.session && !req.session.save) req.session.save = (cb) => cb();
+
+  next();
+});
+
+app.use(flash());
+app.use(methodOverride("_method"));
+
+app.use(passport.initialize());
+app.use(passport.session());
+require("./config/passport");
+
+app.use(fileUpload());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+// Routes in here
+
+app.use((req, res, next) => {
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  res.locals.currentUser = req.user;
+  next();
+});
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.listen(3000, () => console.log(`Server is running on http://localhost:${3000}`));
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("mongodb connected"))
+  .catch((err) => console.log(err));
+
+app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
