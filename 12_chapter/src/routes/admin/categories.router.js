541e3e7026fc760270a50e94ab6ededf2c2db2d7
coolmarvel <marvel97@naver.com>
2023-11-27 20:00:10 +0900

    category list arrange routes & page commit

--- 12_chapter/src/routes/admin/categories.router.js
+++ 12_chapter/src/routes/admin/categories.router.js
@@ -18,12 +18,35 @@ router.post("/add-category", checkAdmin, async (req, res, next) => {
     }
 
     const newCategory = new Category({ title, slug });
-
     await newCategory.save();
+
     req.flash("success", "카테고리가 추가되었습니다.");
     res.redirect("/admin/categories");
   } catch (error) {
     console.log(error);
+    next(error);
+  }
+});
+
+router.get("/", checkAdmin, async (req, res, next) => {
+  try {
+    const categories = await Category.find({});
+    console.log(categories);
+
+    res.render("admin/categories", { categories });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.delete("/:id", checkAdmin, async (req, res, next) => {
+  try {
+    await Category.findByIdAndRemove(req.params.id);
+    req.flash("success", "카테고리가 삭제되었습니다.");
+    res.redirect("/admin/categories");
+  } catch (error) {
+    console.error(error);
     next(error);
   }
 });
