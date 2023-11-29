618c9c03a7a100b317f5b802d70bd0fb2d06e26b
coolmarvel <marvel97@naver.com>
2023-11-29 11:00:27 +0900

    product update & delete router update

--- 12_chapter/src/routes/user/cart/index.js
+++ 12_chapter/src/routes/user/cart/index.js
@@ -1,3 +1,90 @@
 const router = require("express").Router();
 
+const { Product } = require("../../../models/products.model");
+
+router.get("/checkout", (req, res) => {
+  res.render("checkout", { cart: req.session.cart });
+});
+
+router.post("/:product", async (req, res, next) => {
+  const slug = req.params.product;
+
+  try {
+    const product = await Product.findOne({ slug: slug });
+    // 처음 카트에 상품을 넣을 때
+    if (!req.session.cart) {
+      req.session.cart = [];
+      req.session.cart.push({ title: slug, qty: 1, price: product.price, image: `/product-images/${product._id}/${product.image}` });
+    } else {
+      let cart = req.session.cart;
+      let newItem = true;
+
+      // 만약 이미 카트에 있는 상품이라면 한개 추가하고 loop break
+      for (let i = 0; i < cart.length; i++) {
+        if (cart[i].title === slug) {
+          cart[i].qty++;
+          newItem = false;
+          break;
+        }
+      }
+
+      // 처음 추가하는 상품이라면
+      if (newItem) {
+        cart.push({ title: slug, qty: 1, price: product.price, image: `/product-images${product._id}/${product.image}` });
+      }
+    }
+
+    req.flash("success", "상품이 추가되었습니다.");
+    res.redirect("back");
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.get("/update/:product", (req, res) => {
+  const slug = req.params.product;
+  const action = req.query.action;
+
+  let cart = req.session.cart;
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].title === slug) {
+      switch (action) {
+        case "add":
+          cart[i].qty++;
+          break;
+        case "remove":
+          cart[i].qty--;
+          if (cart[i].qty < 1) cart.splice(i, 1);
+          break;
+        case "clear":
+          cart[i].qty++;
+          if (cart.length === 0) delete req.session.cart;
+          break;
+
+        default:
+          console.log("올바른 action을 넣어주세요.");
+          break;
+      }
+    }
+  }
+
+  req.flash("success", "장바구니가 업데이트되었습니다.");
+  res.redirect("back");
+});
+
+router.delete("/", (req, res) => {
+  delete req.session.cart;
+
+  req.flash("success", "장바구니가 비워졌습니다.");
+  res.redirect("back");
+});
+
+router.get("/complete-order", (req, res) => {
+  delete req.session.cart;
+
+  console.log(req.session.cart);
+  res.sendStatus(200);
+});
+
 module.exports = router;
