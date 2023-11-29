620fdf367648f3a9171078026a8af66eec1eeacb
coolmarvel <marvel97@naver.com>
2023-11-29 11:04:53 +0900

    customers products routers get products / categories commit

--- 12_chapter/src/routes/user/products/index.js
+++ 12_chapter/src/routes/user/products/index.js
@@ -1,3 +1,45 @@
 const router = require("express").Router();
 
+const fs = require("fs-extra");
+
+const { Product } = require("../../../models/products.model");
+const { getAllCategories } = require("../../../middleware/product");
+
+router.get("/", getAllCategories, async (req, res, next) => {
+  try {
+    const products = await Product.find();
+
+    res.render("products", { products });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.get("/:category", getAllCategories, async (req, res, next) => {
+  const categorySlug = req.params.category;
+
+  try {
+    const products = await Product.find({ category: categorySlug });
+
+    res.render("products", { products });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.get("/:category/:product", async (req, res, next) => {
+  try {
+    const product = await Product.findOne({ slug: req.params.product });
+    const galleryDir = "src/public/product-images" + product._id + "/gallery";
+    const galleryImages = await fs.readdir(galleryDir);
+
+    res.render("product", { product, galleryImages });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
 module.exports = router;
